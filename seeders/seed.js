94e6ca9a9609f2d885afe0ff4dda1653bf608585
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-10-31 13:07:14 -0700

    modifying for front end

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const db = require('../models');
+let mongoose = require('mongoose');
+let db = require('../models');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
@@ -7,7 +7,7 @@ mongoose.connect('mongodb://localhost/workout', {
   useUnifiedTopology: true,
 });
 
-const workoutSeed = [
+let workoutSeed = [
   {
     day: new Date(new Date().setDate(new Date().getDate() - 9)),
     exercises: [
