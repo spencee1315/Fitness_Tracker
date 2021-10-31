3c001a240bf5885ad37e9df8772c86529bd70cbe
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-10-30 17:52:43 -0700

    added dependencies, initialized middleware, mongoose, connected port

---
+++ server.js
@@ -0,0 +1,30 @@
+// adding required dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan")
+
+const app = express();
+
+// initialize middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// connecting mongoose
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+cont PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+    console.log('Server started on port http://localhost:${PORT}'));
